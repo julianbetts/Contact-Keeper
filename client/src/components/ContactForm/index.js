8173b8efae63cadb8cac0a3d868af7b3710cf912
Julian Betts <wjulianbetts@gmail.com>
2023-02-06 22:09:01 -0800

    appropiated text

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -62,7 +62,7 @@ const ContactForm = () => {
 
   return (
     <div>
-      <h3>What's on your techy mind?</h3>
+      <h3>Creat new Contact</h3>
 
       {Auth.loggedIn() ? (
         <>
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -16,16 +16,8 @@ const Footer = () => {
           </button>
         )}
         <h4>
-          Made with{' '}
-          <span
-            className="emoji"
-            role="img"
-            aria-label="heart"
-            aria-hidden="false"
-          >
-            ❤️
-          </span>{' '}
-          by the Tech Thoughts team.
+          Made 
+          by Group 6
         </h4>
       </div>
     </footer>
